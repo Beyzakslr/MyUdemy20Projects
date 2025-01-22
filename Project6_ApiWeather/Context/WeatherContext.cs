25fe610b653587404e9dcabcc730ab6995e14189
Beyza Kesler <beyzakslr@hotmail.com>
2025-01-22 21:37:13 +0300

    Api

---
+++ Project6_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+    public class WeatherContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial catalog=Db6Project20;integrated Security=true");
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
